e0fbb2f2d2886f45b7320584ed4bd07fe39e70c0
Micheal Klishevich <busation@gmail.com>
2018-02-22 22:11:12 +0300

    simple working model

---
+++ Traffic/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traffic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            var route = new Route(10, 18);
+            var stations = string.Join(",", route.stations.ToArray());
+            Console.Write("aBeginTime = {0}\n" +
+                "aEndTime = {1}\n" +
+                "busInterval = {2} hours\n" +
+                "betweenStationInterval = {3} hours\n" +
+                "stations = {4}\n",
+                route.aBeginTime, route.aEndTime, route.busInterval, route.betweenStationInterval, stations);
+
+            Console.Write("\nEnter current time in hours\n");
+            var time = Console.ReadLine();
+            double currentTime = Convert.ToDouble(time);
+
+            Console.Write("\nEnter station name (Station3)\n");
+            var station = Console.ReadLine();
+
+            var res = route.GetNearestBusTime(currentTime, station);
+            Console.Write(res);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Traffic/Route.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traffic
+{
+    class Route
+    {
+        /// <summary>
+        /// Route has start point A and end point B.
+        /// All time in hours
+        /// </summary>
+        public double aBeginTime;
+        public double aEndTime;
+        public double bBeginTime;
+        public double bEndTime;
+        public double busInterval;
+        public List<string> stations;
+        public double betweenStationInterval;
+
+        public Route(double aBeginTime, double aEndTime)
+        {
+            this.aBeginTime = aBeginTime;
+            this.aEndTime = aEndTime;
+            this.bBeginTime = aBeginTime;
+            this.bEndTime = aEndTime;
+            this.busInterval = 1;
+            this.stations = new List<string> { "Station1", "Station2", "Station3", "Station4" };
+            this.betweenStationInterval = 0.5;
+        }
+
+        public string GetNearestBusTime(double currenTime, string station)
+        {
+            var stationIndex = stations.IndexOf(station);
+            var timeFromPointA = stationIndex * betweenStationInterval;
+            var time = aBeginTime;
+            while (time <= aEndTime)
+            {
+                var stationTime = time + timeFromPointA;
+                if (stationTime >= currenTime)
+                {
+                    return "Nearest bus time - " + stationTime.ToString() + " hours";
+                }
+                time += busInterval;
+            }
+            return "No buses for today";
+        }
+    }
+}
