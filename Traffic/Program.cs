2872df85234fd9626aff1ef0a7885a16c8681d5b
Micheal Klishevich <busation@gmail.com>
2018-03-02 00:16:49 +0300

    serializer example

--- Traffic/Program.cs
+++ Traffic/Program.cs
@@ -11,14 +11,22 @@ namespace Traffic
         static void Main(string[] args)
         {
             Console.Clear();
-            var route = new Route(10, 18);
-            var stations = string.Join(",", route.stations.ToArray());
+            var stations = new List<string> { "Station1", "Station2", "Station3", "Station4" };
+
+            // Create object, just need it once, to serialize into route.xml
+            var routeFromClass = new Route { aBeginTime = 10, aEndTime = 18, bBeginTime = 10, bEndTime = 18, betweenStationInterval = 0.5, busInterval = 1, stations = stations };
+            // Serialize object to xml
+            Serializer.WriteToXmlFile(@"C:\temp\route.xml", routeFromClass);
+            // Create object from file
+            var routeFromFile = Serializer.ReadFromXmlFile<Route>(@"C:\temp\route.xml");
+
+            var stationsPrint = string.Join(",", stations);
             Console.Write("aBeginTime = {0}\n" +
                 "aEndTime = {1}\n" +
                 "busInterval = {2} hours\n" +
                 "betweenStationInterval = {3} hours\n" +
                 "stations = {4}\n",
-                route.aBeginTime, route.aEndTime, route.busInterval, route.betweenStationInterval, stations);
+                routeFromFile.aBeginTime, routeFromFile.aEndTime, routeFromFile.busInterval, routeFromFile.betweenStationInterval, stationsPrint);
 
             Console.Write("\nEnter current time in hours\n");
             var time = Console.ReadLine();
@@ -27,7 +35,7 @@ namespace Traffic
             Console.Write("\nEnter station name (Station3)\n");
             var station = Console.ReadLine();
 
-            var res = route.GetNearestBusTime(currentTime, station);
+            var res = routeFromFile.GetNearestBusTime(currentTime, station);
             Console.Write(res);
 
             Console.ReadLine();
