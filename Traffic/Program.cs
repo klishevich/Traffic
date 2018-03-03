435f556b0651c65a5c029e6595e2c75fc5438ec3
Galina <g-tsoi.n372@mail.ru>
2018-03-03 23:11:47 +0300

    result

--- Traffic/Program.cs
+++ Traffic/Program.cs
@@ -12,16 +12,34 @@ namespace Traffic
     {
         const string FileName = "../../Traffic.txt";
 
-        /*static void SaveRoute(Route route, StreamWriter sw) 
+        /*
+        static void SaveRoute(Route route, StreamWriter sw) 
         {
-            sw.WriteLine($"{route.Id};{route.Name};{route.ABeginTime}; {route.AEndTime};{route.BusInterval};");
+            sw.WriteLine($"{route.Id};{route.Name};{route.ABeginTime};{route.AEndTime};{route.BBeginTime};{route.BEndTime};{route.BusInterval}");
+            sw.WriteLine(route.Stations.Count);
+            foreach (var station in route.Stations) {
+                sw.WriteLine($"{station.Id};{station.Name};{station.StationInterval}");}        
+        }
 
-}*/
+       static void SaveData(List<Route> routes) {
+            using (var sw = new StreamWriter(FileName)) {
+            sw.WriteLine(routes.Count);
+                foreach (var route in routes)
+                    SaveRoute(route, sw);
+            }
+       }
+       */ 
+
+        /// <summary>
+        /// Чтение информации из файла
+        /// </summary>
 
         static Route ReadRoute(StreamReader sr) 
         {
             var line = sr.ReadLine();
             var parts = line.Split(';');
+            if (parts.Length != 7)
+                return null;
 
             var route = new Route 
             {
@@ -29,22 +47,30 @@ namespace Traffic
                 Name = parts[1],
                 ABeginTime = int.Parse(parts[2]),
                 AEndTime = int.Parse(parts[3]),
-                BusInterval = int.Parse(parts[4]),
-                //StationInterval = new int[10].parts[5],
-                //Stations = new List<string>.parts[6],
+                BBeginTime = int.Parse(parts[4]),
+                BEndTime = int.Parse(parts[5]),
+                BusInterval = int.Parse(parts[6]),
+                Stations = new List<Station>()
             };
+
+            int stationNumber = int.Parse(sr.ReadLine());
+            for (int i = 0; i < stationNumber; i++) {
+                line = sr.ReadLine();
+                parts = line.Split(';');
+                if (parts.Length == 3) {
+                    route.Stations.Add(new Station
+                    {
+                        Id = int.Parse(parts[0]),
+                        Name = parts[1],
+                        StationInterval = int.Parse(parts[2])
+                    });
+                }
+            }
             return route;
         }
 
-       /* static void SaveData(List<Route> routes) {
-            using (var sw = new StreamWriter(FileName)) {
-            sw.WriteLine(routes.Count);
-                foreach (var route in routes)
-                    SaveRoute(route, sw);
-            }
-        }
-        */
-        static List<Route> ReadData() {
+        static List<Route> ReadData() 
+        {
             var routesFromFile = new List<Route>();
             using (var sr = new StreamReader(FileName)) {
                 while (!sr.EndOfStream) {
@@ -54,64 +80,25 @@ namespace Traffic
             }
             return routesFromFile;
         }
+        
+        /// <summary>
+        /// Сортировка по времени прибытия автобуса
+        /// </summary>
+        
+        static int SortTime(Time time1, Time time2)
+        {
+            return time1.Wait - time2.Wait;
+        }
 
         static void Main(string[] args)
         {      
+            //SaveData(routes);
 
-           
-            List<Route> routes = new List<Route>()
-                {
-                    new Route
-                    {
-                        Id = 0,
-                        Name = "Route 372",
-                        ABeginTime = 300,
-                        AEndTime = 1439,
-                        BusInterval = 10,
-                        BBeginTime = 301,
-                        BEndTime = 1431,
-                        StationInterval = new int[] {3,5,4},
-                        Stations = new List<string> {"A","B","C","D"}
+        /// <summary>
+        /// Считывание из файла
+        /// </summary>
 
-                        /*Stations = new List<Station>()
-                        {
-                            new Station
-                            {
-                                Id = 0,
-                                Name = "A",
-                            },
-                            new Station
-                            {
-                                Id = 1,
-                                Name = "B",
-                            },
-                            new Station
-                            {
-                                Id = 2,
-                                Name = "C",
-                            },
-                            new Station
-                            {
-                                Id = 3,
-                                Name = "D",
-                            }
-                        }*/
-                    },
-                    new Route
-                    {
-                        Id = 1,
-                        Name = "Route 103",
-                        ABeginTime = 305,
-                        AEndTime = 1430,
-                        BBeginTime = 302,
-                        BEndTime = 1432,
-                        BusInterval = 5,
-                        StationInterval = new int[] {6,5,3,2},
-                        Stations = new List<string> {"Q","W","Z","X","A"}
-                    }
-                }; 
-            
-            /*List<Route> routes;
+            List<Route> routes;
                 try {
                 routes = ReadData();
             }
@@ -119,48 +106,212 @@ namespace Traffic
                 Console.WriteLine("Error reading file data");
                 return;
             }
-            Console.WriteLine(routes);
-            Console.ReadLine();
-                //SaveData(routes);*/
-              /*  List<Route> routes = new List<Route>;
-                ReadData();*/
 
-            //добавить считование файла
-            
-           do
-	        {
-            Console.Clear();
-            //var route = new Route(300, 1439);
-                /*var stations = string.Join(",", route.stations.ToArray());
-               Console.Write("aBeginTime = {0}\n minutes" +
-                "aEndTime = {1}\n minutes" +
-                "busInterval = {2} minutes\n" +
-                "betweenStationInterval = {3} minutes\n" +
-                "stations = {4}\n",
-                routes[0].ABeginTime, routes[0].AEndTime, routes[0].BusInterval, routes[0].StationInterval, routes[0].Stations);*/
-
-                /*Console.Write("\nEnter current time in hours\n");//nowTime
-                var time = Console.ReadLine();
-                double currentTime = Convert.ToDouble(time);*/
-                
+            //Console.Clear();
+            do 
+            {    
                 DateTime today = DateTime.Now;
-                Console.WriteLine("Current time: {0:t}", today);//timeOfDay
+                Console.WriteLine("\nCurrent time: {0:t}", today);//timeOfDay
 
-                Console.Write("\nEnter station name (A, Z): \n");
-                var station = Console.ReadLine();
+                Console.Write("\nEnter station name (For example, A): ");
+                Console.ForegroundColor = ConsoleColor.Green;    
+                string station = Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.Gray;    
+                Console.WriteLine();
 
-                for (int i = 0; i< routes.Count; i++) 
-                {
-                    if (routes[i].Stations.IndexOf(station)>=0) //if station exists in this route
-                        {
-                        var res = routes[i].GetNearestBusTime(station);
-                        Console.WriteLine(res);
+                List<Time> times = new List<Time> {};
+                int indexStation = -1;
+
+                    for (int i = 0; i< routes.Count; i++) 
+                    {
+                        for (int j = 0; j < routes[i].Stations.Count; j++)
+			            {
+                            if (station == routes[i].Stations[j].Name)
+                                {
+                                    int indexOfLastStation = routes[i].Stations.Count-1;
+                                    indexStation = routes[i].Stations[j].Id;
+                        
+                                    /// <summary>
+                                    /// Если пользователь вводит станцию, которая есть в маршруте,
+                                    /// то по ее индексу программа находит наименьшее время
+                                    /// до каждой из конечных станций.
+                                    /// </summary>
+
+                                    int resFirst = routes[i].GetNearestBusTimeFirst(indexStation);
+                                    int resLast = routes[i].GetNearestBusTimeLast(indexStation);
+                            
+                                    times.Add(new Time() {RouteName = routes[i].Name, Wait = resFirst, Name = routes[i].Stations[0].Name});
+                                    times.Add(new Time() {RouteName = routes[i].Name, Wait = resLast, Name = routes[i].Stations[indexOfLastStation].Name});
+			                    }
+    			        }
                     }
-                }
 
-                Console.WriteLine("Для повтора нажмите Enter");
-                } while (Console.ReadKey().Key==ConsoleKey.Enter);
-            
+                /// <summary>
+                /// Если станция есть в одном из маршрутов,
+                /// то она была добавлена в список.
+                /// Происходит сортировка этого списка
+                /// по наименьшему времени ожидания и выводится на экран.
+                ///
+                /// Если снация не найдена, то выводится соответствущие сообщение на экран.
+                /// </summary>
+
+                if (times.Count !=0)
+                {
+                    times.Sort(SortTime);
+                    foreach (var time in times)
+                        if(station !=time.Name)
+                            Console.WriteLine("{0, -9}: Nearest bus to Station {1,1} will be in {2,1} minutes", time.RouteName, time.Name, time.Wait);
+                }
+                    else {
+                         Console.WriteLine("Station {0} is not found! Please, try again!", station);
+                         };
+
+             Console.WriteLine("\nДля повтора нажмите Enter");
+            } while (Console.ReadKey().Key==ConsoleKey.Enter);
+           Console.ReadKey();
+            /* 
+            List<Route> routes = new List<Route>()
+                {
+                    new Route
+                    {
+                        Id = 0,
+                        Name = "Route 372",
+                        ABeginTime = 360,
+                        AEndTime = 1490,
+                        BusInterval = 10,
+                        BBeginTime = 361,
+                        BEndTime = 1491,
+                        //StationInterval = new int[] {3,5,4}, // it works
+                        //Stations = new List<string> {"A","B","C","D"}
+
+                        Stations = new List<Station>()
+                        {
+                            new Station
+                            {
+                                Id = 0,
+                                Name = "A",
+                                StationInterval = 6,
+                            },
+                            new Station
+                            {
+                                Id = 1,
+                                Name = "B",
+                                StationInterval = 3,
+                            },
+                            new Station
+                            {
+                                Id = 2,
+                                Name = "C",
+                                StationInterval = 4,
+                            },
+                            new Station
+                            {
+                                Id = 3,
+                                Name = "D",
+                                StationInterval = 0,
+                            }
+                        }
+                    },
+                    new Route
+                    {
+                        Id = 1,
+                        Name = "Route 103",
+                        ABeginTime = 365,
+                        AEndTime = 1480,
+                        BBeginTime = 362,
+                        BEndTime = 1492,
+                        BusInterval = 5,
+                        //StationInterval = new int[] {6,5,3,2},
+                        //Stations = new List<string> {"Q","W","Z","X","A"}
+                        Stations = new List<Station>()
+                        {
+                            new Station
+                            {
+                                Id = 0,
+                                Name = "Q",
+                                StationInterval = 6,
+                            },
+                            new Station
+                            {
+                                Id = 1,
+                                Name = "W",
+                                StationInterval = 4,
+                            },
+                            new Station
+                            {
+                                Id = 2,
+                                Name = "R",
+                                StationInterval = 5,
+                            },
+                            new Station
+                            {
+                                Id = 3,
+                                Name = "Z",
+                                StationInterval = 3,
+                            },
+                            new Station
+                            {
+                                Id = 4,
+                                Name = "C",
+                                StationInterval = 0,
+                            }
+                        }
+                    },
+                    new Route
+                    {
+                        Id = 2,
+                        Name = "Route 3",
+                        ABeginTime = 242,
+                        AEndTime = 1492,
+                        BusInterval = 10,
+                        BBeginTime = 374,
+                        BEndTime = 1484,
+                        //StationInterval = new int[] {3,5,4}, // it works
+                        //Stations = new List<string> {"A","B","C","D"}
+
+                        Stations = new List<Station>()
+                        {
+                            new Station
+                            {
+                                Id = 0,
+                                Name = "A",
+                                StationInterval = 6,
+                            },
+                            new Station
+                            {
+                                Id = 1,
+                                Name = "B",
+                                StationInterval = 3,
+                            },
+                            new Station
+                            {
+                                Id = 2,
+                                Name = "F",
+                                StationInterval = 9,
+                            },
+                            new Station
+                            {
+                                Id = 3,
+                                Name = "V",
+                                StationInterval = 7,
+                            },
+                            new Station
+                            {
+                                Id = 4,
+                                Name = "N",
+                                StationInterval = 4,
+                            },                            new Station
+                            {
+                                Id = 5,
+                                Name = "M",
+                                StationInterval = 0,
+                            },
+
+                        }
+                    },
+                }; 
+            */
         }
+
     }
 }
--- Traffic/Route.cs
+++ Traffic/Route.cs
@@ -6,104 +6,108 @@ using System.Threading.Tasks;
 
 namespace Traffic
 {
-    /*class Station
+    /// <summary>
+    /// Номер маршрута,
+    /// название,
+    /// интервал до следующей станции
+    /// </summary>
+    class Station
 	{
         public int Id { get; set; }
         public string Name { get; set; }
-        //public int StationInterval { get; set; }
+        public int StationInterval { get; set; }
+	}
 
-        public Station(int id, string name)
-        {
-            Id = id;
-            Name = name;
-        }
+    /// <summary>
+    /// Название маршрута,
+    /// время ожидания,
+    /// конечная точка,
+    /// </summary>
 
-        public Station()
-        {
+    class Time
+    {
+        public string RouteName {get; set; }
+        public int Wait { get; set; }
+        public string Name { get; set; }
+    }
 
-        }
-	}*/    
+    /// <summary>
+    /// Номер маршрута,
+    /// название,
+    /// время первого рейса из первой станции,
+    /// время последнего рейса из первой станции,
+    /// время первого рейса из последней станции,
+    /// время последнего рейса из последней станции,
+    /// интервал между автобусами,
+    /// список станции
+    /// </summary>
+
     class Route
     {
-        /// <summary>
-        /// Route has start point A and end point B.
-        /// All time in hours
-        /// </summary>
         public int Id {get; set;}
         public string Name { get; set; }
-        public int ABeginTime {get; set;}//int fromfile 5:00 => 300, from file
-        public int AEndTime {get; set;}//int from file 24:00 => 1439, from file
+        public int ABeginTime {get; set;}
+        public int AEndTime {get; set;}
         public int BBeginTime {get; set;}
         public int BEndTime {get; set;}
-        //public int bBeginTime;//int from file 
-        //public int bEndTime;//int from file 
-        public int BusInterval{get; set;}//int from file = 10
-        public List<string> Stations { get; set; }
-        public int[] StationInterval { get; set; }
-        //public List<string> stations;
-        //public int betweenStationInterval;// int in file = 5, from file according to Station Index from 0 (to number of station +1)*/
+        public int BusInterval{get; set;}
+        public List<Station> Stations { get; set; }
 
-        public Route(int id, string name, int aBeginTime, int aEndTime, int busInterval, List<string> stations, int[] stationInterval)
-        {
-            Id=id;
-            Name = name;
-            ABeginTime = aBeginTime;
-            AEndTime = aEndTime;
-            //this.bBeginTime = aBeginTime;
-            //this.bEndTime = aEndTime;
-            BusInterval = busInterval; //10 minutes, from file 
-            Stations = stations;
-            //{ "Station1", "Station2", "Station3", "Station4" };//from class Station
-            StationInterval = stationInterval;// 5 minutes, from file
-        }
         
-        public Route()
+        /// <summary>
+        /// Метод, который вначале по индексу станции
+        /// считает интервал до конечной точки вообще.
+        /// 
+        /// Потом обращается к другому методу и возвращает 
+        /// интервал ожидания автобуса с учетом настоящего времени.
+        /// </summary>
+
+        public int GetNearestBusTimeFirst(int indexStation)
         {
-
-        }
-
-
-        /*public Station GetStationByName(Route routes, string givenStationName)
-        {
-            Station ourStation;
-            foreach (var route in routes)
-                foreach (var station in stations)
-                    if (givenStationName==route.station.Name)
-                    {
-                        ourStation = this.route.station;
-                    }
-            else return "Do not understand you!";
-        }*/
-
-        public string GetNearestBusTime(string station)
-        {
-            int stationIndex = Stations.IndexOf(station);
             int timeFromPointA = 0;
+            int indexOfLastStation = Stations.Count-1;
+           
+            for (int i = 0; i < indexStation; i++)
+			{
+                timeFromPointA +=Stations[i].StationInterval;
+			}
+            return GetNearestTime(ABeginTime, AEndTime, timeFromPointA, indexOfLastStation); 
+        }
+        /// <summary>
+        /// Метод, который вначале по индексу станции
+        /// считает интервал до начальной точки вообще.
+        /// 
+        /// Потом обращается к другому методу и возвращает 
+        /// интервал ожидания автобуса с учетом настоящего времени.
+        /// </summary>
+
+        public int GetNearestBusTimeLast(int indexStation)
+        {
             int timeFromPointB = 0;
             int indexOfLastStation = Stations.Count-1;
-            
-            //if (stationIndex<0) return ;
-                    
-            //int stationIndex = stations.IndexOf(station);//getting by index, will be changed!!! instead srations.IndexOf put stations.Id
-            //int timeFromPoint = stationIndex * betweenStationInterval;//both parametrs should be in file; interval between stations will be random
-           
-            for (int i = 0; i < stationIndex; i++)
-			{
-                timeFromPointA +=StationInterval[i];
-			}
 
-            for (int i = indexOfLastStation; i > stationIndex; i--)
+            for (int i = indexOfLastStation; i > indexStation; i--)
 			{
-                timeFromPointB +=StationInterval[indexOfLastStation-1];
+                timeFromPointB +=Stations[i-1].StationInterval;
 			}
-            GetNearestTime(AbeginTime, AendTime, timeFromPointA, indexOfLastStation);
-            GetNearestTime(BbeginTime, BendTime, timeFromPointB, Stations[0]);
+            return GetNearestTime(BBeginTime, BEndTime, timeFromPointB, 0); 
         }
 
-        public string GetNearestTime(int beginTime, int endTime, int timeFromPoint, string indexOfLastStation) //в процессе
+        /// <summary>
+        /// Метод, который переводит текущее время в целочисленный тип.
+        /// Затем учитывает интервалы между автобусами
+        /// и считает непосредственно время прибытия автобуса на введенную станцию.
+        /// Потом из этого значения времени вычитается значение текущего 
+        /// и выводится время, которое надо ждать прибытия автобуса.
+        /// </summary>
+
+        public int GetNearestTime(int beginTime, int endTime, int timeFromPoint, int indexOfLastStation) //в процессе
         {
             DateTime currentTimeInDateTime = DateTime.Now;
             int currenTime = currentTimeInDateTime.Hour*60 + currentTimeInDateTime.Minute;
+
+            if (currenTime<60)
+                currenTime = 1440+currenTime;
 
             int time = beginTime;
             while (time <= endTime)
@@ -113,12 +117,11 @@ namespace Traffic
                 if (stationTime >= currenTime)
                 {
                     int timeNeedToWait = stationTime - currenTime;
-                    return "Nearest bus to Station "+ indexOfLastStation+ " will be in - " + timeNeedToWait.ToString() + " minutes";
+                    return timeNeedToWait;
                 }
                 time += BusInterval;
             }
-            return "No buses for today";
-           }
+          return 0;
         }
-        
     }
+}
